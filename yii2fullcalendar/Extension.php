bddc8c1094e4d104302d8b7baefeed489b56bd08
Philipp Frenzel <philipp@frenzel.net>
2013-10-24 14:36:47 +0200

    stupid stupid error of myself

--- yii2fullcalendar/Extension.php
+++ yii2fullcalendar/Extension.php
@@ -22,6 +22,6 @@ class Extension extends \yii\base\Extension
    */
   public static function init()
   {
-    Yii::setAlias('@yii2fullcalendar;', __DIR__);
+    Yii::setAlias('@yii2fullcalendar', __DIR__);
   }
 }
