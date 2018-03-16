a3ab591c6457093bb4d47444b09f2a882aa8f586
akorinek <korinek@mcmaster.ca>
2018-03-16 09:28:59 -0400

    Create SchedulerAsset.php
    add scheduler

---
+++ SchedulerAsset.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace yii2fullcalendar;
+
+use Yii;
+use yii\web\AssetBundle;
+
+/**
+ * @link http://www.frenzel.net/
+ * @author Philipp Frenzel <philipp@frenzel.net>
+ */
+
+class SchedulerAsset extends AssetBundle
+{
+    /**
+     * [$js description]
+     * @var array
+     */
+    public $js = [
+        'scheduler.js',
+    ];
+}
